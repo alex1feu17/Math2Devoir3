ce8036caae1976061642e59aa19c75349941a570
alex1feu17 <alexyd@hotmail.ca>
2022-12-11 15:15:37 -0500

    Avancement des fonctions

--- MathematiqueDevoir3/Program.cs
+++ MathematiqueDevoir3/Program.cs
@@ -5,37 +5,62 @@ namespace MathematiqueDevoir3
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Menu ");
-           Console.WriteLine("1- Entrez la clef de transposition :");
-           Console.WriteLine("2- Allez chercher le document incluant le message :");
-           Console.WriteLine("3- Afficher le message chiffré :");
-           Console.WriteLine("4- Afficher le message déchiffré :");
+           string key = " ";
+           string message = " ";
+           string option;
 
-           Console.WriteLine("Selectionné votre option :");
-           string number = Console.ReadLine();
+           do
+           {
+            
+               Console.Clear();
+               Console.WriteLine("Menu ");
+               Console.WriteLine("1- Entrez la clef de transposition :");
+               Console.WriteLine("2- Allez chercher le document incluant le message :");
+               Console.WriteLine("3- Afficher le message chiffré :");
+               Console.WriteLine("4- Afficher le message déchiffré :");
+               Console.WriteLine("Selectionné votre option :");
 
-            switch (number)
-            {
-                case "1":
-                    Console.WriteLine("Entrez la clé de transposition :");
-                    string key = Console.ReadLine();                   
+           
 
-                    break;
-                case "2":
-                    Console.WriteLine("Chercher le document incluant le message :");                    
+                switch (option = Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Entrez la clé de transposition :");
+                        key = Console.ReadLine();                   
 
-                    break;
-                case "3":
-                    Console.WriteLine("Message chiffré :");
+                        break;
+                    case "2":
+                        try
+                        {
+                            Console.WriteLine("Entrez le nom du fichier :");
 
-                    break;
-                case "4":
-                    Console.WriteLine("Message déchiffré :");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                                    
 
-                    break;
-                default:
-                    break;
-            }
+                        break;
+                    case "3":
+                        Console.WriteLine("Message chiffré :");
+
+                        break;
+                    case "4":
+                        Console.WriteLine("Message déchiffré :");
+
+                        break;
+                    default:
+                        break;
+                }
+
+                if (!option.Equals("5"))
+                {
+                    Console.WriteLine("\nAppuyez sur une touche pour continuer...");
+                    Console.ReadLine();
+                }
+
+            }while (option != "5");
         }
     }
 }
