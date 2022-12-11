1f39e8ecf4ed923862b6093b9b87a776625bdf2c
alex1feu17 <alexyd@hotmail.ca>
2022-12-11 13:57:17 -0500

    Squelette du projet

---
+++ MathematiqueDevoir3/Chiffrement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematiqueDevoir3
+{
+    static class Chiffrement
+    {
+        static String Chiffrer(String message, String cle)
+        {
+            return " ";
+        }
+        static String Dechiffrer(String message, String cle)
+        {
+            return " ";
+        }
+    }
+}
