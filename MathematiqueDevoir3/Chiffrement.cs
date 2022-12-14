7e96fee05baa61df381611b2a43a2d49a84479e6
alex1feu17 <alexyd@hotmail.ca>
2022-12-13 22:25:05 -0500

    methode StringToByte

--- MathematiqueDevoir3/Chiffrement.cs
+++ MathematiqueDevoir3/Chiffrement.cs
@@ -12,12 +12,24 @@ namespace MathematiqueDevoir3
 {
     static class Chiffrement
     {
+        public static int StringToByte(String message)
+        {
+            int y = 0;
+
+            for (int i = message.Length-1; i >=0; i--)
+            {
+               y+=((int)message.ElementAt<char>(i)) * (int)Math.Pow(2,(i-message.Length+1));
+            }
+            Console.Write(y);
+            return y;
+        }
         public static String Chiffrer(String message, String cle)
         {
             int nbCol = 0;
             int nbLi = 0;
             int compt = 0;
             int compt2 = 0;
+            string vi;
             
             string messageChiffrer = " ";
             
@@ -42,20 +54,27 @@ namespace MathematiqueDevoir3
             compt = 1;
 
             // Chiffrer le message a l'aide du tableau
-            while (compt != cle.Length+1)
+            while (compt != cle.Length + 1)
             {
                 if (compt.ToString() == cle[compt2].ToString())
                 {
-                    for (int i = 0; i < nbLi; i++)                   
+                    for (int i = 0; i < nbLi; i++)
                         messageChiffrer = messageChiffrer + tabMessage[i, compt2];
-                    
+
                     compt++;
                     compt2 = 0;
                 }
                 else
                     compt2++;
             }
+
+            Console.WriteLine("Entrez la valeur du vecteur d'initialisation :");
+            vi = Console.ReadLine();
+
             
+
+         
+
             return messageChiffrer;
         }
         public static String Dechiffrer(String message, String cle)
@@ -63,22 +82,23 @@ namespace MathematiqueDevoir3
             int nbCol = 0;
             int nbLi = 0;
             int compt = 0;
+            cle = Regex.Replace(cle, @"\s", "");
             nbCol = cle.Length;
             nbLi = message.Length / nbCol;
-            char[,] tabMessage = new char[nbLi, nbCol];
+            char[,] tabdefmessage = new char[nbLi, nbCol];
 
             // Transporte le message chiffrer dans un tableau 2D
-            /*for (int i = 0; i < nbCol; i++)
+            for (int i = 0; i < nbCol; i++)
             {
                 for (int y = 0; y < nbLi; y++)
                 {
                     if (compt != message.Length)
                     {
-                        tabMessage[i, y] = message[compt];
+                        tabdefmessage[i, y] = message[compt];
                         compt++;
                     }
                 }
-            }*/
+            }
             return "D";
         }
     }
