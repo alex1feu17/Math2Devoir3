64a85f4b8fcc9c7a4d007221caa32f38760e814f
alex1feu17 <alexyd@hotmail.ca>
2022-12-13 23:59:39 -0500

    Avancement du chiffrement abec tableau binaire

--- MathematiqueDevoir3/Chiffrement.cs
+++ MathematiqueDevoir3/Chiffrement.cs
@@ -14,6 +14,7 @@ namespace MathematiqueDevoir3
     {
         public static int StringToByte(String message)
         {
+
             int y = 0;
 
             for (int i = message.Length-1; i >=0; i--)
@@ -29,11 +30,15 @@ namespace MathematiqueDevoir3
             int nbLi = 0;
             int compt = 0;
             int compt2 = 0;
-            string vi;
-            
+            int NUM = 31;
+            byte[] vi = new byte[1];
+            vi[0] = 00110;
+
+
             string messageChiffrer = " ";
-            
-                        
+           
+
+
             cle = Regex.Replace(cle, @"\s", "");
             nbCol = cle.Length;
             nbLi = message.Length / nbCol;
@@ -68,12 +73,28 @@ namespace MathematiqueDevoir3
                     compt2++;
             }
 
-            Console.WriteLine("Entrez la valeur du vecteur d'initialisation :");
-            vi = Console.ReadLine();
+            /*Console.WriteLine("Entrez la valeur du vecteur d'initialisation :");
+            vi = Console.ReadLine();*/
 
+            int[] messagevalue = new int[messageChiffrer.Length];
+            byte[] messageBite = new byte[messageChiffrer.Length];
+
+            for (int i = 0; i < messageChiffrer.Length; i++)           
+                messagevalue[i] = messageChiffrer[i] & NUM;
+
+            for (int i = 0; i < messageChiffrer.Length; i++)           
+                messageBite[i] =  (byte)messagevalue[i];
+
+            for (int i = 0; i < messageChiffrer.Length; i++)
+            {
+                if (i == 0)
+                    messagevalue[i] = (messagevalue[i] ^ vi[0]) + (byte)00001;
+                else
+                    messagevalue[i] = (messagevalue[i] ^ messagevalue[i - 1]) + (byte)00001;
+
+                Console.WriteLine(messagevalue[i]);
+            }
             
-
-         
 
             return messageChiffrer;
         }
